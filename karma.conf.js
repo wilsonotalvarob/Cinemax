cbdf0873b8b25876636b010e4d39db9f869a149b
Wilson <wilsonotalvaro@gmail.com>
2021-04-25 04:17:23 -0500

    Single Run true

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
